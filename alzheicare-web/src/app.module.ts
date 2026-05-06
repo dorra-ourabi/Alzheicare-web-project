74171fc94de004feaf86ca11f16af8327b0b8117
dorra dorra <dorra.ourabi@insat.ucar.tn>
2026-05-06 10:27:57 +0100

    setted the project structure

--- alzheicare-web/src/app.module.ts
+++ alzheicare-web/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CalendarModule } from './calendar/calendar.module';
+import { MailModule } from './mail/mail.module';
+import { ChatModule } from './chat/chat.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, CalendarModule, MailModule, ChatModule, DashboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
