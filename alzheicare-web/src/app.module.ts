6031a7855498161143aa9c0b66cc01d78b4ff98d
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2026-05-27 00:02:05 +0100

    Import the telgram module in AppModule

--- alzheicare-web/src/app.module.ts
+++ alzheicare-web/src/app.module.ts
@@ -7,6 +7,7 @@ import { InvitationModule } from './invitation/invitation.module.js';
 import { AuthModule } from './auth/auth.module.js';
 import { CalendarModule } from './calendar/calendar.module.js';
 import { MailModule } from './mail/mail.module.js';
+import { TelegramModule } from './telegram/telegram.module.js';
 import { ChatModule } from './chat/chat.module.js';
 import { DashboardModule } from './dashboard/dashboard.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
@@ -22,6 +23,7 @@ import { StripeModule } from './stripe/stripe.module.js';
     InvitationModule,
     CalendarModule,
     MailModule,
+    TelegramModule,
     ChatModule,
     DashboardModule,
     EventEmitterModule.forRoot(),
